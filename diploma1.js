e0a0f29d8c06f479a1279a3e1432b05550be5f29
all of it <138558451+ollienotolly@users.noreply.github.com>
2023-07-23 19:44:19 +0300

    Add files via upload
    добавила ещё одну игру, обновила код в игре с яблоками, добавила диалоговые всплывающие окна, добавила css анимацию яблок

--- diploma1.js
+++ diploma1.js
@@ -18,9 +18,47 @@ setInterval(animation, 30);
 
 houseIMG.onclick = function() {
     window.location.href = 'diploma2.html';
+    document.getElementById('d').style.display = 'none';
 };
 
 appleIMG.onclick = function() {
     window.location.href = 'diploma3.html';
 };
+
+document.getElementById('a').style.display = 'none';
+document.getElementById('kc').style.display = 'none';
+document.getElementById('kb').style.display = 'none';
+document.getElementById('kc').style.display = 'none';
+document.getElementById('kd').style.display = 'none';
+
+function bla() {
+     document.getElementById('a').style.display = 'block';
+     document.getElementById('ka').style.display = 'none';
+     document.getElementById('kb').style.display = 'block';
+}
+
+document.getElementById('b').style.display = 'none';
+function bla2() {
+    document.getElementById('b').style.display = 'block';
+    document.getElementById('kb').style.display = 'none';
+    document.getElementById('kc').style.display = 'block';
+  }
+
+document.getElementById('c').style.display = 'none';
+function bla3() {
+    document.getElementById('c').style.display = 'block';
+    document.getElementById('kc').style.display = 'none';
+    document.getElementById('kd').style.display = 'block';
+  }
+
+document.getElementById('d').style.display = 'none';
+function bla4() {
+    document.getElementById('d').style.display = 'block';
+    document.getElementById('kd').style.display = 'none';
+  }
+
+/* document.getElementById('').style.display = 'none';
+function bla() {
+    document.getElementById('').style.display = 'block';
+  } */
 
--- diploma3.js
+++ diploma3.js
@@ -14,37 +14,38 @@ function stopApple1() {
     apple1.style.left = left;
     apple2.style.visibility = "visible";
     btn.addEventListener("click", stopApple2);
-
-    function stopApple2() {
-        btn.addEventListener("click", stopApple2);
-        let left = window.getComputedStyle(apple2).getPropertyValue("left");
-            apple2.classList.remove("animate");
-            apple2.style.left = left;
-        apple3.style.visibility = "visible";
-        btn.addEventListener("click", stopApple3);
-
-        function stopApple3() {
-            let left = window.getComputedStyle(apple3).getPropertyValue("left");
-            apple3.classList.remove("animate");
-            apple3.style.left = left;
-            apple4.style.visibility = "visible";
-            btn.addEventListener("click", stopApple4);
-
-            function stopApple4() {
-                let left = window.getComputedStyle(apple4).getPropertyValue("left");
-                apple4.classList.remove("animate");
-                apple4.style.left = left;
-                apple5.style.visibility = "visible";
-                btn.addEventListener("click", stopApple5);
-
-                function stopApple5() {
-                    let left = window.getComputedStyle(apple5).getPropertyValue("left");
-                    apple5.classList.remove("animate");
-                    apple5.style.left = left;
-                }
-            }
-        }
-
-    }
-
 }
+
+function stopApple2() {
+    btn.addEventListener("click", stopApple2);
+    let left = window.getComputedStyle(apple2).getPropertyValue("left");
+    apple2.classList.remove("animate");
+    apple2.style.left = left;
+    apple3.style.visibility = "visible";
+    btn.addEventListener("click", stopApple3);
+    document.getElementById("restart").style.display = "block";
+}
+
+function stopApple3() {
+    let left = window.getComputedStyle(apple3).getPropertyValue("left");
+    apple3.classList.remove("animate");
+    apple3.style.left = left;
+    apple4.style.visibility = "visible";
+    btn.addEventListener("click", stopApple4);
+}
+
+function stopApple4() {
+    let left = window.getComputedStyle(apple4).getPropertyValue("left");
+    apple4.classList.remove("animate");
+    apple4.style.left = left;
+    apple5.style.visibility = "visible";
+    btn.addEventListener("click", stopApple5);
+}
+
+function stopApple5() {
+    let left = window.getComputedStyle(apple5).getPropertyValue("left");
+    apple5.classList.remove("animate");
+    apple5.style.left = left;
+}
+
+
